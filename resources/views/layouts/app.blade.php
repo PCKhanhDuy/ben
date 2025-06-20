0c4a2d6a67ba47a89aaada7afe23e0fee58f0a82
Duy Phan Chau Khanh <duypck@daico-furniture.com>
2025-06-20 16:03:54 +0700

    change layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,14 @@
 <!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ben Studio - Photography Portfolio and Services">
+    <meta name="keywords" content="photography, studio, Ben, photo, portfolio">
+    <meta name="author" content="Ben Studio">
+
     <title>@yield('title', 'Home Page')</title>
     {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet"> --}}
     <link href="https://fonts.googleapis.com/css2?family=Gwendolyn&display=swap" rel="stylesheet">
@@ -12,6 +18,13 @@
     <link rel="stylesheet" href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('vendor/aos/css/aos.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    
+    <link rel="apple-touch-icon" href="{{ secure_asset('images/main-logo.jpg') }}">
+    <link rel="icon" type="image/png" href="{{ secure_asset('images/main-logo.jpg') }}">
+
+    <!-- ✅ Thêm preload nếu bạn muốn speed up Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     @yield('head')
 </head>
 
